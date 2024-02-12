84f6e7b7b48a5228470750720c22883ac851f30d
David Madrigal <dajosemm12@gmail.com>
2024-02-11 18:22:01 -0600

    Agregando una sola linea de código que falto
    Signed-off-by: David Madrigal <dajosemm12@gmail.com>

--- Tareas/TAREA_CINCO/src/main.cpp
+++ Tareas/TAREA_CINCO/src/main.cpp
@@ -3,6 +3,7 @@
 int main() {
     // Ejecutar el script Python desde la carpeta especificada
     system("python ../TAREA_CINCO/src/ManejoArchivos.py");
+    system("python ../TAREA_CINCO/src/Estudiantes.py");
 
     return 0;
 }
